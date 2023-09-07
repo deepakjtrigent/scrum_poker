539abfdcf0dbbc9b93d8bfe1e39524b197d6e36a
Himanshu <139129418+himanshuTrigent@users.noreply.github.com>
2023-09-07 10:28:38 +0530

    resolve conflicts

--- FE/src/app/shared/services/toast.service.ts
+++ FE/src/app/shared/services/toast.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
-export const toastState =  {
+export const toastState = {
   success: 'success',
-  danger: 'danger'
-}
+  danger: 'danger',
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToastService {
   constructor(private snackBar: MatSnackBar) {}
-  public showToast(message: string, toastState : string): void {
+  public showToast(message: string, toastState: string): void {
     const config: MatSnackBarConfig<any> = {
       duration: 60000,
       panelClass: [toastState],
       horizontalPosition: 'center',
       verticalPosition: 'top',
-      data : [message]
+      data: [message],
     };
 
     this.snackBar.open(message, undefined, config);
