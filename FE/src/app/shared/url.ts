4dadb1e8c6909ac8d29aef0b94c848c1b2127440
Himanshu Salhotra <himanshu_sa@blr.trigent.com>
2024-01-01 12:52:04 +0530

    changed port to 5000 in url

--- FE/src/app/shared/url.ts
+++ FE/src/app/shared/url.ts
@@ -1,7 +1,7 @@
 export const scrumPokerUrls = {
-  createRoomUrl: 'http://134.209.147.133:8000/create_room',
-  roomUrls: `http://134.209.147.133:8000/room`,
-  websocketUrl: 'ws://134.209.147.133:8000/room'
+  createRoomUrl: 'http://134.209.147.133:5000/create_room',
+  roomUrls: `http://134.209.147.133:5000/room`,
+  websocketUrl: 'ws://134.209.147.133:5000/room'
 };
 
 // export const scrumPokerUrls = {
